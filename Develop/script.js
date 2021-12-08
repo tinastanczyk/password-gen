c1046e21d86c7b4a75826b0238e408964d394a3d
Tina Stanczyk <cstanczyk123@gmail.com>
2021-12-08 09:59:41 -0500

    Started functions that randomize my arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specStr = "!'()*+,-./:;<=>?@[^_`{|}~";
+var numStr = "0123456789";
+var letterStr = "abcdefghijklmnopqrstuvwxyz";
+var letterArr = letterStr.split('');
+var numArr = numStr.split('');
+var specArr = specStr.split('');
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +16,32 @@ function writePassword() {
 
 }
 
+function generateLetter(){
+  var randomLetter = Math.floor(Math.random() * letterArr.length);
+  var chosenLetter = letterArr[randomLetter];
+
+  return chosenLetter;
+}
+
+function generateNumber(){
+  var randomNum = Math.floor(Math.random() * numArr.length);
+  var chosenNum = numArr[randomNum];
+
+  return chosenNum;
+}
+
+function generateSpec(){
+  var randomSpec = Math.floor(Math.random() * specArr.length);
+  var chosenSpec = specArr[randomSpec];
+
+  return chosenSpec;
+}
+
+function generatePassword() {
+
+  var randomPassword = chosenLetter.concat(chosenNum).concat(chosenSpec);
+
+  return randomPassword;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
